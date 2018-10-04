68a73b4249a82ccd474fdf2f72d9a82793e5ed2e
SungHoon <ksh920610@gmail.com>
2018-10-04 19:41:48 +0900

    [방탈출] 캐릭터 이동, 카메라 회전 업그레이드

---
+++ DreamWeaver/Assets/EscapeRoom/PlayerController/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+    public float rotSpeed = 3.0f;
+    public float jumpSpeed = 250.0f;
+
+    // Update is called once per frame
+    void Update () {
+        //앞뒤 이동
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+        
+        //좌우 회전
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * rotSpeed);
+
+        //점프
+        if (Input.GetButtonDown("Jump") && transform.position.y < 1.1f)
+        {
+            transform.GetComponent<Rigidbody>().AddForce(0, jumpSpeed, 0);
+        }
+    }
+}

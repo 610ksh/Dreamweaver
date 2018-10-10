87f1ab25cda63f625aa88b4288bbee2121002fda
Sunghoon <ksh920610@gmail.com>
2018-10-10 13:35:18 +0900

    [플랫포머] JumperCube(점프대) 2차 구현

---
+++ DreamWeaver/Assets/Platformer/JumperCube/Script/Jumper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    public float force = 50f;
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider c) //occur when player get in
+    {
+        if (c.tag == "Player")
+        {
+            Force();
+        }
+
+    }
+
+    void Force()
+    {
+        player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        player.GetComponent<Rigidbody>().AddForce(new Vector3(0, 2, 0) * force, ForceMode.Impulse);
+    }
+}

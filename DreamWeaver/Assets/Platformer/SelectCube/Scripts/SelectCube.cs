7895b8d3725060d7a2755c1ed875088e4bf93ee0
Sunghoon <ksh920610@gmail.com>
2018-10-12 14:34:53 +0900

    [플랫포머] SelectCube 3차 구현(콜라이더 제거 - 밟은것도 다시 밟을수 있음)

--- DreamWeaver/Assets/Platformer/SelectCube/Scripts/SelectCube.cs
+++ DreamWeaver/Assets/Platformer/SelectCube/Scripts/SelectCube.cs
@@ -2,45 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectCube : MonoBehaviour
-{
+public class SelectCube : MonoBehaviour {
     public GameObject disappearingWall;
 
     bool isAllChildSelected = false;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
         //자식 큐브들이 모두 선택되었는지 검사 
-        if (isAllChildSelected)
+        if(isAllChildSelected)
         {
             DisappearWall();
             Debug.Log("all child selected");
         }
-        else
+        else 
         {
             int count = 0;
             for (int i = 0; i < transform.childCount; ++i)
             {
 
-                if (this.transform.GetChild(i).GetComponent<SelectCubeEle>().isSelected)
+                if(this.transform.GetChild(i).GetComponent<SelectCubeEle>().isSelected)
                 {
-                    ++count;
+                    ++count; 
                 }
                 if (count == transform.childCount) isAllChildSelected = true;
             }
         }
-    }
+	}
     void DisappearWall()
     {
         //public으로 받아온 벽을 밀어버린다
-        disappearingWall.transform.Translate(0, 0, 2);
+        if(disappearingWall)
+            disappearingWall.transform.Translate(-1, 0, 0);
+        Destroy(disappearingWall, 1);
     }
 
 }

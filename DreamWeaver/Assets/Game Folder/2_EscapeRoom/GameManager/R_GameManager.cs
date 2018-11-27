11fe657401e84ccc74743857549b9be9b99b9bd0
SungHoon <ksh920610@gmail.com>
2018-11-27 14:48:47 +0900

    [방탈출] MessageUI 추가 / FadeEffect 일반화 작업 완성

---
+++ DreamWeaver/Assets/Game Folder/2_EscapeRoom/GameManager/R_GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class R_GameManager : MonoBehaviour
+{
+
+    // 싱글톤 적용
+    public static R_GameManager instance;
+
+    // KeyPad 이미지
+    public GameObject keypadUI;
+
+    // Player
+    public GameObject player;
+    R_Player_Movement playerMovement;
+
+    // 메인 빛
+    public GameObject mainLight;
+
+    // 불 버튼
+    public GameObject lightButtonUI;
+    RectTransform lightButtonPos;
+
+    // Fade Effect UI
+    public GameObject fadeEffectUI;
+    // Fade Effect 현재 실행 여부 변수
+    bool isFade = false;
+
+    // fadeIn 변수
+    GameObject fadeIn;
+
+    // MessageUI
+    public GameObject messageUI;
+
+    void Awake()
+    {
+        // 싱글톤
+        instance = this;
+        playerMovement = player.GetComponent<R_Player_Movement>();
+        playerMovement.SetMove(false);
+    }
+
+    private void Start()
+    {
+        // FadeIn 캐싱
+        fadeIn = fadeEffectUI.transform.GetChild(0).gameObject;
+        // FadeIn 실행
+        fadeIn.SetActive(true);
+        fadeEffectUI.transform.GetChild(0).GetComponent<FadeEffect>().StartFade();
+        
+        // 전등 버튼 UI 위치값 변수
+        lightButtonPos = lightButtonUI.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        // fadeIn이 끝나면
+        if(!fadeIn.activeSelf)
+        {
+            // 플레이어 이동을 풀어준다.
+            playerMovement.SetMove(true);
+        }
+
+        // 13m 밑으로 떨어지면 FadeOut 시작
+        if (player.transform.position.y < -13f)
+        {
+            if (!isFade)
+            {
+                isFade = true;
+                fadeEffectUI.transform.GetChild(1).gameObject.SetActive(true);
+                fadeEffectUI.transform.GetChild(1).GetComponent<FadeEffect>().StartFade();
+            }
+        }
+
+        // 80m 밑으로 떨어지면 플랫포머로
+        if (player.transform.position.y < -80f)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    // 불 버튼 위치 설정
+    public void SetLightButtonPos(Vector3 pos)
+    {
+        lightButtonPos.position = pos;
+    }
+
+    // 최초 메시지 출력 함수
+    public void StartDialog()
+    {
+        // 컨트롤러에 있는 
+        messageUI.gameObject.SetActive(true);
+        messageUI.GetComponent<MessageUIController>().StartDialog();
+    }
+
+}
---
+++ DreamWeaver/Assets/Game Folder/2_EscapeRoom/MessageUI/MessageDialogController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageDialogController : MonoBehaviour {
+
+    //  변수
+    public float typingDelay = 0.1f;
+    public float skipDelay = 0.2f;
+
+    // 전체 대화창의 내용 (문자배열)
+    string[] fulltext;
+
+    // 전체 대화창의 개수
+    int dialog_cnt;
+
+    // 현재 대화창 위치
+    int cnt;
+
+    // 현재 대화창의 전체 내용
+    string currentText;
+
+    // 현재 텍스트 타이핑 상태 (full : 끝, cut : 딜레이 생략)
+    bool text_full;
+    bool text_cut;
+
+    Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // 최초 텍스트 시작호출
+    public void Get_Typing()
+    {
+        //재사용을 위한 변수초기화
+        text_full = false;
+        text_cut = false;
+        cnt = 0;
+
+        // 메시지 내용 받아오기
+        fulltext = transform.GetComponent<MessageObject>().GetDialog();
+
+        // 퀴즈 전체 내용 개수 받아오기
+        dialog_cnt = fulltext.Length;
+
+        //타이핑 코루틴시작
+        StartCoroutine(ShowText(fulltext));
+    }
+
+    IEnumerator ShowText(string[] _fullText)
+    {
+        // 모든 텍스트 종료
+        if (cnt >= dialog_cnt)
+        {
+            //text_exit = true;
+            // 코루틴 중단
+            StopCoroutine("ShowText");
+            // MessageUI 비활성화
+            transform.parent.parent.gameObject.SetActive(false);
+        }
+        else
+        {
+            //기존문구clear
+            currentText = "";
+            
+            //타이핑 시작
+            for (int i = 0; i < _fullText[cnt].Length; i++)
+            {
+                //타이핑중도탈출
+                if (text_cut == true)
+                {
+                    break;
+                }
+
+                //현재 텍스트에 한글자씩 추가로 적기
+                currentText = _fullText[cnt].Substring(0, i + 1);
+                text.text = currentText;
+                yield return new WaitForSeconds(typingDelay);
+            }
+
+            // 타이핑이 끝나면 모든 문자출력
+
+            //Debug.Log("Typing 종료");
+            if(text_full == false)
+            {
+                text.text = _fullText[cnt];
+                yield return new WaitForSeconds(skipDelay);
+            }
+
+            //스킵_지연후 종료
+            text_full = true;
+        }
+    }
+
+    // 다음 버튼 함수와 연결짓기
+    public void Next_Typing()
+    {
+        // 현재 텍스트 모두 출력하고 버튼 클릭시
+        if (text_full == true)
+        {
+            cnt++;
+            text_full = false;
+            text_cut = false;
+            // 왜 이렇게 하는거지?
+            StartCoroutine(ShowText(fulltext));
+        }
+        //텍스트 타이핑 생략
+        else
+        {
+            text_cut = true;
+        }
+    }
+
+    public void PriorText()
+    {
+        // 이전 텍스트가 없을때
+        if(cnt==0)
+        {
+            Debug.Log("뒤로가기 더이상 불가");
+            return;
+        }
+        // 이전 텍스트가 있을때
+        else
+        {
+            cnt--;
+            text_full = false;
+            text_cut = false;
+            StartCoroutine(ShowText(fulltext));
+        }
+    }
+}
